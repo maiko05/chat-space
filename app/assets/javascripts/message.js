a059f2a46f00eb6b29ae2bba45c8eea0efc6d753
maiko05 <maikorto5@icloud.com>
2019-07-06 11:36:29 +0900

    automatic-updating.update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,17 @@
+$(document).on('turbolinks:load', function() { 
+
 $(function(){
   function buildHTML(message){
     var image =``;
     if (message.image) image += `<img class="lower-message__image" src=${message.image} >`
       var html = 
-       `<div class="message" data-message-id=${message.id}>
+       `<div class="message" data-message-id="${message.id}">
           <div class="upper-info">
             <div class="upper-info__user">
               ${message.user_name}
             </div>
             <div class="upper-info__date">
-              ${message.date}
+              ${message.created_at}
             </div>
           </div>
           <div class="lower-message">
@@ -45,4 +47,30 @@ $(function(){
     })
     return false;
   });
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message_id");
+    group_id = $('.current-group').data("group_id")
+    $.ajax({
+      url: `/groups/${group_id}/api/messages/`,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        messages.forEach(function(message) {
+         insertHTML += buildHTML(message);
+         $('.messages').append(insertHTML);
+        });
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      };
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
+});
 });
